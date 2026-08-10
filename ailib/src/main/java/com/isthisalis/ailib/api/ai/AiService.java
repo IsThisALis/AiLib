94d39420f3eea29f81eb83a5e5e264888e5ccd10
IsThisALis <gamemodebronze2@gmail.com>
2026-08-10 10:40:59 +0500

    docs(AiCaller): add javadoc

--- ailib/src/main/java/com/isthisalis/ailib/api/ai/AiService.java
+++ ailib/src/main/java/com/isthisalis/ailib/api/ai/AiService.java
@@ -10,8 +10,26 @@ import lombok.NonNull;
  */
 public interface AiService {
 
+  /**
+   * Should update data in implementation.
+   * 
+   * @param config New configuration.
+   * @param ToolCallParser Tool Calling implementation.
+   */
   public void update(@NonNull Configuration config, ToolCallParser ToolCallParser);
 
+  /**
+   * Sends message to AI model.
+   * 
+   * @param message Message to be sent.
+   * @return AI response as String.
+   */
   public String ask(@NonNull String message);
+
+  /**
+   * Sends request to AI model
+   * @param json JSON to be sent to AI model.
+   * @return AI response as JSON String.
+   */
   public String request(@NonNull String json);
 }
